fc16b2e8308ce7e85bd1d12f8fdb7dd317fe705d
todd edwards <yewtreestudios@hotmail.co.uk>
2024-06-13 10:33:08 +0000

    added checkout views and templates

--- checkout/forms.py
+++ checkout/forms.py
@@ -3,24 +3,25 @@ from .models import Order
 
 class OrderForm(forms.ModelForm):
     class Meta:
-        model = Orderfields = ('full_name', 'email', 'phone_number',
-                               'street_address1', 'street_address2',
-                               'town_or_city', 'postcode', 'country',
-                               'county')
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',
+                  'county',)
 
-    def __init__(sefl, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         """
-            add placeholders and classes, remove auto-generated 
-            labels and set autofocus on first field
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
         """
         super().__init__(*args, **kwargs)
         placeholders = {
             'full_name': 'Full Name',
-            'email': 'email',
-            'phone_number': 'phone number',
-            'country': 'country',
-            'postcode': 'postcode',
-            'town_or_city': 'Town Or City',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
             'county': 'County',
@@ -29,7 +30,7 @@ class OrderForm(forms.ModelForm):
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if self.fields[field].required:
-                placeholder = f'{placeholder[field]} *'
+                placeholder = f'{placeholders[field]} *'
             else:
                 placeholder = placeholders[field]
             self.fields[field].widget.attrs['placeholder'] = placeholder
