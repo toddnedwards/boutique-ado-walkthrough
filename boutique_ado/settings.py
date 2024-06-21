d864d90b4a6a593ef0114d5774aadff2ca4c8e94
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-06-21 09:08:38 +0000

    updated allauth templates and tested profiles

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -61,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'boutique_ado.urls'
