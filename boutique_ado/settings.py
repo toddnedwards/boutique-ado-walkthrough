ea65ad863bd49c9d740d9564733611099c531e1d
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-06-22 08:37:22 +0000

    added confirmation emails

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,6 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['8000-toddnedward-boutiqueado-sbau53e9atk.ws.codeinstitute-ide.net']
 
+DEFAULT_AUTO_FIELD='django.db.models.AutoField'
 
 # Application definition
 
@@ -179,3 +180,4 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
