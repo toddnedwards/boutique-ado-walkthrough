e09836ed8aaf3fae4cdd0b675238202e8335451e
Todd Edwards <yewtreestudios@hotmail.co.uk>
2024-06-25 14:44:47 +0000

    updated bucket name in settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -181,7 +181,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # bucket config
-    AWS_STORAGE_BUCKET_NAME = 'manage-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-walkthrough-todd'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
